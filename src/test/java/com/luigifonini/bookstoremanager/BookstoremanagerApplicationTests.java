d29b88f0707d2eb15cea6cf21a3c0b5876245776
Luigi Calovi Fonini <luitows@gmail.com>
2024-12-03 18:29:42 -0300

    first commit: project structure created

---
+++ src/test/java/com/luigifonini/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigifonini.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
